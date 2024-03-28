67d40eec7d29590f1322fa77e87150dfb5dc3137
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:08:53 +0300

    generate team-employee component

---
+++ src/app/team-employee/team-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-employee.component.html',
+  styleUrl: './team-employee.component.css'
+})
+export class TeamEmployeeComponent {
+
+}
