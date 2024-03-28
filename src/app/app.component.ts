a210aa8b98e918cf6fe3bc3990925be784f4aa40
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:49:27 +0300

    implement app-root component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
+import { ServicesComponent } from './services/services.component';
+import { SupportComponent } from './support/support.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PricelistComponent } from './pricelist/pricelist.component';
+import { CasesComponent } from './cases/cases.component';
+import { TeamComponent } from './team/team.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ClientsComponent } from './clients/clients.component';
+import { RequestFormComponent } from './request-form/request-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ThumbnailComponent,
+    ServicesComponent,
+    SupportComponent,
+    ExperienceComponent,
+    PricelistComponent,
+    CasesComponent,
+    TeamComponent,
+    ReviewsComponent,
+    ClientsComponent,
+    RequestFormComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'app';
 }
