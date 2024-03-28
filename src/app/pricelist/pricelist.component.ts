428c83f9534a66259d01cb37a7a42d58caa65171
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:05:44 +0300

    generate pricelist component

---
+++ src/app/pricelist/pricelist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricelist',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricelist.component.html',
+  styleUrl: './pricelist.component.css'
+})
+export class PricelistComponent {
+
+}
