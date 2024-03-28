343d369cf77807e8f807964bfd519d4d55b2f7c9
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:00:20 +0300

    generate thumbnail component

---
+++ src/app/thumbnail/thumbnail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail',
+  standalone: true,
+  imports: [],
+  templateUrl: './thumbnail.component.html',
+  styleUrl: './thumbnail.component.css'
+})
+export class ThumbnailComponent {
+
+}
