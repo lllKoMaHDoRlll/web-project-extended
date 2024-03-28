a052c446cff319d6a076b0957004c82f0a0d5a61
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:04:36 +0300

    generate support-card component

---
+++ src/app/support-card/support-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-card.component.html',
+  styleUrl: './support-card.component.css'
+})
+export class SupportCardComponent {
+
+}
