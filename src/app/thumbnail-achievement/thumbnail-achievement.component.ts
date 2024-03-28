17bccfe800da829aece9fb520d9a0c46345b1f5f
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:01:14 +0300

    generate thumbnail-achievement component

---
+++ src/app/thumbnail-achievement/thumbnail-achievement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail-achievement',
+  standalone: true,
+  imports: [],
+  templateUrl: './thumbnail-achievement.component.html',
+  styleUrl: './thumbnail-achievement.component.css'
+})
+export class ThumbnailAchievementComponent {
+
+}
