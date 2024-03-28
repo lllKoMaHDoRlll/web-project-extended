7d25e374a5083358b16cc2dbc0510ddd870348c1
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:06:20 +0300

    generate pricelist-card component

---
+++ src/app/pricelist-card/pricelist-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricelist-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricelist-card.component.html',
+  styleUrl: './pricelist-card.component.css'
+})
+export class PricelistCardComponent {
+
+}
