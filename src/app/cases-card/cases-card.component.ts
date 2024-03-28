9c59f9f534196733032238d66b3d80847b20b834
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-28 22:07:36 +0300

    generate cases-card component

---
+++ src/app/cases-card/cases-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cases-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './cases-card.component.html',
+  styleUrl: './cases-card.component.css'
+})
+export class CasesCardComponent {
+
+}
