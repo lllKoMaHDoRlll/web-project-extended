1e3be5d43d6896b6f27beed03659fb3107412a70
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-30 19:26:18 +0300

    implement support card component

--- src/app/support-card/support-card.component.ts
+++ src/app/support-card/support-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SupportCard } from '../supportcard';
 
 @Component({
   selector: 'app-support-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './support-card.component.html',
   styleUrl: './support-card.component.css'
 })
 export class SupportCardComponent {
-
+  @Input() supportCard!: SupportCard;
 }
