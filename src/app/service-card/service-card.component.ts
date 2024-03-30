2bccb83dde4cfea45d8d758fd82ee34e64d604b5
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-30 18:58:38 +0300

    implement serviceCard component

--- src/app/service-card/service-card.component.ts
+++ src/app/service-card/service-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiceCard } from '../servicecard';
+
 
 @Component({
   selector: 'app-service-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css'
 })
-export class ServiceCardComponent {
 
+export class ServiceCardComponent {
+  @Input() serviceCard!: ServiceCard;
 }
