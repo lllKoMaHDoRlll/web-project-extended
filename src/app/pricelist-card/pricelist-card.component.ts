10307aa82aec88db01abf4c57847b83b600c7e21
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-03-30 20:02:38 +0300

    implement pricelist card component

--- src/app/pricelist-card/pricelist-card.component.ts
+++ src/app/pricelist-card/pricelist-card.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PricelistCard } from '../pricelist-card';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-pricelist-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './pricelist-card.component.html',
   styleUrl: './pricelist-card.component.css'
 })
 export class PricelistCardComponent {
+  @Input() pricelistCard!: PricelistCard;
+  faCheck = faCheck;
 
+  parseCardIsMain() {
+    if (this.pricelistCard.isMainOption) {
+      return "main-option option";
+    }
+    else {
+      return "option"
+    }
+  }
 }
