1e36a395d22d14dcbced0092dbd3441dd597526d
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-04-03 19:49:09 +0300

    implement team employee component

--- src/app/team-employee/team-employee.component.ts
+++ src/app/team-employee/team-employee.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { TeamEmployee } from './../team-employee';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-team-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './team-employee.component.html',
   styleUrl: './team-employee.component.css'
 })
 export class TeamEmployeeComponent {
-
+  @Input() teamEmployee!: TeamEmployee;
 }
