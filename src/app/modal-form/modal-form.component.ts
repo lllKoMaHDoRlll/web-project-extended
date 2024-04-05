5edde56042eb2f75f6a6e376e1eb431a77132051
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-04-05 22:01:36 +0300

    implement modal form component

--- src/app/modal-form/modal-form.component.ts
+++ src/app/modal-form/modal-form.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { RequestFormComponent } from '../request-form/request-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeadset } from '@fortawesome/free-solid-svg-icons';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-modal-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    FontAwesomeModule,
+    MatBottomSheetModule
+  ],
   templateUrl: './modal-form.component.html',
   styleUrl: './modal-form.component.css'
 })
 export class ModalFormComponent {
+  faHeadset = faHeadset;
 
+  constructor (private _bottomSheet: MatBottomSheet) {
+
+  }
+
+  openModalForm(): void {
+    this._bottomSheet.open(ModalFormSheet)
+  }
+}
+
+@Component({
+  selector: 'modal-form-sheet',
+  template: `
+    <app-request-form></app-request-form>
+  `,
+  standalone: true,
+  imports: [
+    RequestFormComponent
+  ]
+})
+export class ModalFormSheet {
+  constructor(private _bottomSheetRef: MatBottomSheetRef<ModalFormSheet>) {}
 }
