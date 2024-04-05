6485cb2ba6c9c17465246b85ea8928af552f9a60
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-04-05 13:12:05 +0300

    implement form component

--- src/app/request-form/request-form.component.ts
+++ src/app/request-form/request-form.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormHandlerService } from '../form-handler.service';
 
 @Component({
   selector: 'app-request-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './request-form.component.html',
-  styleUrl: './request-form.component.css'
+  styleUrl: './request-form.component.css',
 })
 export class RequestFormComponent {
+  formHandlerService: FormHandlerService = inject(FormHandlerService);
 
+  applyForm = new FormGroup({
+    name: new FormControl(localStorage.getItem('user-name')),
+    phone: new FormControl(localStorage.getItem('user-phone')),
+    email: new FormControl(localStorage.getItem('user-email')),
+    message: new FormControl(localStorage.getItem('user-message')),
+    acception: new FormControl(localStorage.getItem('user-acception')),
+  });
+
+  submitApplication() {
+    this.formHandlerService.submitApplication(this.applyForm);
+  }
+
+  onChange(event: any) {
+    localStorage.setItem(event.target.id, event.target.value);
+  }
 }
