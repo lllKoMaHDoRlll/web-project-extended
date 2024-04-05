372a3a5359e1726ee4343a26b83f7ef2ab76cfbc
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-04-05 19:12:03 +0300

    generate modal form component

---
+++ src/app/modal-form/modal-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-form.component.html',
+  styleUrl: './modal-form.component.css'
+})
+export class ModalFormComponent {
+
+}
